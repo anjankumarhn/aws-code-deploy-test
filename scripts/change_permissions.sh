0f08c8df3ce86c239f48d093914d061a936bd8d5
Anjan Kumar H N <anjankumarhn@users.noreply.github.com>
2018-07-04 12:19:47 +0530

    change_permissions.sh
    change_permissions.sh

---
+++ scripts/change_permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#mkdir /usr/share/nginx/html/{media,var,skin}
+#find /usr/share/nginx/html -type f -exec chmod 644 {} \;
+#find /usr/share/nginx/html -type d -exec chmod 755 {} \;
+#chmod -R 777 /usr/share/nginx/html/media
+chmod -R 777 /var/www/html/
